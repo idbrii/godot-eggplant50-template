91bc8066ddcb7cadcd94c5b1d687033eaa597a9a
David Briscoe <idbrii@gmail.com>
2023-12-29 22:32:58 -0800

    movement2: Zoom camera out, use ScreenCamera
    Since movement2 was built for 1080p, it looks bad at our tiny res. Zoom
    out so you can see the height of the level as it was intended.
    
    Switch to ScreenCamera since it's a much nicer presentation.
    
    Simplify the camera code and make the two cameras both accept a node
    path.

--- games/movement2/Camera/FollowCamera.gd
+++ games/movement2/Camera/FollowCamera.gd
@@ -1,18 +1,22 @@
 extends Camera2D
 
 
-var Target
+export var target_node : NodePath
+
+onready var _target = get_node_or_null(target_node)
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	Target = get_tree().get_nodes_in_group("Player")
-	if Target != null: 
-		Target = Target[0] #because get_nodes_in_group returns an array even if it's an array of size 1
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	if Target != null:
-		position = Target.position
+	if _target:
+		# Only follow x position because we're doing weird zooming.
+		position.x = _target.position.x
 	else:
 		print("FollowCamera doesn't have a target")
+		_grab_target()
+
+
+func _grab_target():
+	var players = get_tree().get_nodes_in_group("Player")
+	if players != null: 
+		_target = players[0] #because get_nodes_in_group returns an array even if it's an array of size 1
+
--- games/movement2/Camera/ScreenCamera.gd
+++ games/movement2/Camera/ScreenCamera.gd
@@ -1,26 +1,30 @@
 extends Camera2D
 
-# Camera script follwing a target (usually the player)
+# Camera script following a target (usually the player) between rooms.
 # This camera is snapped to a grid, therefore only moves when the character exits a screen.
 
-export var target : NodePath
+export var target_node : NodePath
 export var align_time : float = 0.2
 export var screen_size := Vector2(1920, 1080)
 
-onready var Target = get_node_or_null(target)
-onready var Twee = $Tween
+onready var _target = get_node_or_null(target_node)
 
 func _physics_process(_delta: float) -> void:
-	if not is_instance_valid(Target):
-		var targets: Array = get_tree().get_nodes_in_group("Player")
-		if targets: Target = targets[0]
-	if not is_instance_valid(Target):
+	if not _target:
+		_grab_target()
+	if not _target:
 		return
 	
 	# Actual movement
 	var tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUINT)
 	tween.tween_property(self, "global_position", desired_position(), align_time)
 
-# Calculating the gridnapped position
+# Calculating the gridsnapped position
 func desired_position() -> Vector2:
-	return (Target.global_position / screen_size).floor() * screen_size + screen_size/2
+	return (_target.global_position / screen_size).floor() * screen_size + screen_size/2
+
+func _grab_target():
+	var players = get_tree().get_nodes_in_group("Player")
+	if players != null: 
+		_target = players[0] #because get_nodes_in_group returns an array even if it's an array of size 1
+
