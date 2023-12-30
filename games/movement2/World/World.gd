307446ed29138ca72231931eb2ff3255ead6f0ce
David Briscoe <idbrii@gmail.com>
2023-12-29 21:38:34 -0800

    Move toggle fullscreen to its own binding
    Avoid toggling it when pressing ui_cancel.

--- games/movement2/World/World.gd
+++ games/movement2/World/World.gd
@@ -1,9 +1,5 @@
 extends Node2D
-# Switching between fullscreen and not fullscreen by pressing esc
 
 func _input(_event: InputEvent) -> void:
-	if Input.is_action_just_pressed("ui_cancel"):
-		OS.window_fullscreen = !OS.window_fullscreen
-
 	if Input.is_action_just_pressed("pause"):
 		Eggplant.return_to_menu()
--- mainmenu/eggplant.gd
+++ mainmenu/eggplant.gd
@@ -2,6 +2,13 @@ extends Node
 
 const _main_menu = preload("res://mainmenu/mainmenu.tscn")
 
+
+func _input(_event: InputEvent):
+	# Toggle fullscreen with Alt-Enter
+	if Input.is_action_just_pressed("toggle_fullscreen"):
+		OS.window_fullscreen = !OS.window_fullscreen
+
+
 func transition_to(next_scene: PackedScene):
 	get_tree().change_scene_to(next_scene)
 
