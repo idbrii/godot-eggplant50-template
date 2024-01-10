07e2b3792cdd5683460d861778b9b59728cb407e
David Briscoe <idbrii@gmail.com>
2024-01-09 23:03:46 -0800

    Make FollowCamera not follow so closely
    Give a deadzone around the middle so the player can move a bit. There
    are lots of opportunities for improvement, but I think ScreenCamera
    works better.

--- games/movement2/Camera/FollowCamera.gd
+++ games/movement2/Camera/FollowCamera.gd
@@ -2,14 +2,24 @@ extends Camera2D
 
 
 export var target_node : NodePath
+export var max_player_offset_from_center := 75.0
+export var prevent_vertical_movement := false
 
 onready var _target = get_node_or_null(target_node)
 
 
 func _process(_delta):
 	if _target:
-		# Only follow x position because we're doing weird zooming.
-		position.x = _target.position.x
+		# Lots of potential improvement for camera logic. For inspiration see:
+		# https://www.gamedeveloper.com/design/scroll-back-the-theory-and-practice-of-cameras-in-side-scrollers
+		var orig_y = position.y
+		var delta = position - _target.position
+		delta = delta.limit_length(max_player_offset_from_center)
+		position = _target.position + delta
+
+		if prevent_vertical_movement:
+			position.y = orig_y
+
 	else:
 		print("FollowCamera doesn't have a target")
 		_grab_target()
