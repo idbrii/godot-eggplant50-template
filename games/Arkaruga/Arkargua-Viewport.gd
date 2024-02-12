163c7aa1ea2d95dc53dedd8ca6a83d91fa299558
Jon Gill <jonagill@gmail.com>
2024-02-12 15:50:23 -0800

    Get object shadows working in Arkaruga

--- games/Arkaruga/Arkargua-Viewport.gd
+++
@@ -1,7 +0,0 @@
-extends Viewport
-
-func _ready():
-	var parent = get_parent()
-	var parentWorld = parent.get_world_2d()
-	self.world_2d = parentWorld
-	pass
---
+++ games/Arkaruga/Scripts/Arkargua-Viewport.gd
@@ -0,0 +1,5 @@
+extends Viewport
+
+func _ready():
+	world_2d = get_tree().get_root().world_2d;
+	pass
