8a30953dd3d77b3aa9d6429266d11ad2f536c893
family PC <bittner.w@gmail.com>
2024-02-12 23:00:39 -0800

    split baseball and bat logic states

--- games/bones7242/scripts/baseball.gd
+++ games/bones7242/scripts/baseball.gd
@@ -20,19 +20,6 @@ var max_fall_speed = 100
 
 var h_move_speed = 8
 
-# variables for swinging
-var bat_x = 640 / 2
-var bat_y = 320 / 2
-var start_swing_x = 0
-var swing_x = start_swing_x
-var start_swing_y = 0
-var swing_y = start_swing_y
-var bat_start_radius = 32
-var bat_radius = bat_start_radius
-var color = Color(1,0,0)
-const GREEN = Color( 0, 1, 0, 1 )
-var swing_center : Vector2
-
 func change_state(new_state):
 	# store the current state as last state, if it exists
 	if current_state:
@@ -84,30 +71,6 @@ func _process(delta):
 			if next_movement <= max_fall_speed:
 				position.y += next_movement # move ball the ball
 			
-			# listen for swing
-			# draw circle
-			var windup = Input.is_action_pressed("action1")
-			var swing = Input.is_action_just_released("action1")
-			
-			if windup:
-				#make circle bigger
-				bat_radius += 2
-				#pull circle back on an arc
-				swing_x += 10
-				swing_y += 5
-				print('winding up bat:' + str(bat_radius))
-			
-			if swing:
-				print('SWING!' + str(bat_radius))
-				#reset vars
-				bat_radius = bat_start_radius
-				swing_x = start_swing_x
-				swing_y = start_swing_y
-			
-			#draw circle
-			swing_center = Vector2(bat_x - swing_x, bat_y - swing_y)
-			#print('swing_center:' + str(swing_center))
-			
 			# also listen for hitting the ground
 			if position.y >= 300:
 				change_state(State.GROUNDED)
@@ -118,13 +81,6 @@ func _process(delta):
 				change_state(State.HELD)
 				position.y = 199
 		_:
-			print("I am not a state I know of!")
+			print("I am not a baseball state I know of!")
 		
 	#print('power:' + str(power))
-
-func _draw():
-	#test
-	draw_circle(Vector2(120,120), 10, GREEN)
-	#draw bat circle
-	print('swing_center draw:' + str(swing_center))
-	draw_circle(swing_center, bat_radius, color)
--- games/bones7242/scripts/bat.gd
+++ games/bones7242/scripts/bat.gd
@@ -1,2 +1,107 @@
 extends Node2D
 
+const State = {IDLE = 0, WINDUP = 1, SWING = 2, FOLLOWTHROUGH = 3}
+# Access values with State.HELD, etc.
+
+
+var starting_windup_speed = 10
+var windup_speed = starting_windup_speed
+var max_windup_speed = 100
+var initial_power = 0
+var power = initial_power
+var power_increment = 1
+var max_power = 100
+
+# variables for swinging
+var bat_x = 640 / 2
+var bat_y = 320 / 2
+var start_swing_x = 0
+var swing_x = start_swing_x
+var max_swing_x = 300
+var start_swing_y = 0
+var swing_y = start_swing_y
+var max_swing_y = 100
+var bat_start_radius = 32
+var bat_radius = bat_start_radius
+var color = Color(1,0,0)
+const GREEN = Color( 0, 1, 0, 1 )
+var swing_center : Vector2
+
+var initial_state = State.IDLE
+var current_state : int
+var last_state : int
+
+var swing_x_swing_increment : int
+var swing_y_swing_increment : int
+
+func change_state(new_state):
+	# store the current state as last state, if it exists
+	if current_state:
+		last_state = current_state
+	
+	current_state = new_state
+	
+	pass
+
+func _ready():
+	change_state(initial_state)
+	pass
+	
+func _process(delta):
+	
+	match current_state:
+		State.IDLE:
+			#print("i am HELD")
+			#wait around
+			#listen for change to windup
+			if Input.is_action_pressed("action2"):
+				change_state(State.WINDUP)
+				print("i am winding up the bat")
+		State.WINDUP:
+			# do windup stuff
+			#make circle bigger
+			bat_radius += 2
+			#change x and y to pull circle back on an arc
+			swing_x += (max_swing_x - swing_x) / 20
+			swing_y += (max_swing_y - swing_y) / 20
+			print('winding up bat:' + str(bat_radius))
+			# build up power
+			if power < max_power:
+				power += power_increment
+			#listen for change to released
+			if Input.is_action_just_released("action2"):
+				print("i am SWING the bat")
+				print('swing power:' + str(power))
+				change_state(State.SWING)
+				#reset vars
+				bat_radius = bat_start_radius
+				swing_x = start_swing_x
+				swing_y = start_swing_y
+				# set vars for swing
+				var speed = 200 - power
+				swing_x_swing_increment = (swing_x - start_swing_x) / speed
+				swing_y_swing_increment = (swing_y - start_swing_y) / speed
+		State.SWING:
+			# do swing stuff
+			bat_radius -= 2
+			swing_x -= swing_x_swing_increment
+			swing_y -= swing_y_swing_increment
+			# listen for swing complete
+			if swing_x <= start_swing_x:
+				change_state(State.FOLLOWTHROUGH) #should go to follow through.
+		State.FOLLOWTHROUGH:
+			# tbd: move ball up and left, hold for a moment
+			# when done, go back to idle
+			change_state(State.IDLE)
+			
+		_:
+			print("I am not a bat state I know of!")
+		
+	#print('power:' + str(power))
+	update()
+
+func _draw():
+	#draw bat circle
+	swing_center = Vector2(bat_x - swing_x, bat_y - swing_y)
+	print('swing_center draw:' + str(swing_center))
+	draw_circle(swing_center, bat_radius, color)
