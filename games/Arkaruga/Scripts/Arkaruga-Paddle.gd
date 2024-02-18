02757336fa80960dfd711db2ecdd0a5b754ee850
Jon Gill <jonagill@gmail.com>
2024-02-18 12:25:31 -0800

    Update Arkaruga scripts to preferred Godot style

--- games/Arkaruga/Scripts/Arkaruga-Paddle.gd
+++ games/Arkaruga/Scripts/Arkaruga-Paddle.gd
@@ -1,15 +1,20 @@
 extends KinematicBody2D
 
-export var maxSpeed = 450
-export var acceleration = 300
-export var reverseAcceleration = 450
-export var boostModifier = 1.6
-export var maxBounceAngle = 80
+export var maxSpeed : float = 450
+export var acceleration : float = 300
+export var reverseAcceleration : float = 450
+export var boostModifier : float = 1.6
+export var maxBounceAngle : float = 80
 
-var speed = 0
+var _speed : float = 0
 
 func _ready():
 	pass
+	
+func _input(event):
+	if event.is_action_pressed("action1"):
+		print("Toggle!")
+		# toggle();
 
 func _process(delta):
 	var direction = 0
@@ -23,37 +28,37 @@ func _process(delta):
 	
 	if direction == 0:
 		# slow back down to 0
-		speed = move_toward(speed, 0, acceleration * delta)
+		_speed = move_toward(_speed, 0, acceleration * delta)
 	else:
 		var activeAcceleration = 0
-		if abs(speed) > 0 && sign(direction) != sign(speed):
+		if abs(_speed) > 0 && sign(direction) != sign(_speed):
 			activeAcceleration = reverseAcceleration * modifier
 		else:
 			activeAcceleration = acceleration * modifier
 		
-		speed += direction * activeAcceleration * delta
+		_speed += direction * activeAcceleration * delta
 	
 	var activeMaxSpeed = maxSpeed * modifier
-	speed = clamp(speed, -activeMaxSpeed, activeMaxSpeed)
+	_speed = clamp(_speed, -activeMaxSpeed, activeMaxSpeed)
 	
 	
-	var collision = move_and_collide(Vector2.RIGHT * speed * delta)
+	var collision = move_and_collide(Vector2.RIGHT * _speed * delta)
 	if collision != null:
-		if collision.collider.has_method("isBall"):
+		if collision.collider.is_in_group("Balls"):
 			# don't let the ball stop us
 			# we should be able to fix this via collision layers/masks
 			# but until 4.0 it's impossible to do one-way collision detection. Fun!
 			position += collision.remainder
 		else:
 			# stop our velocity
-			speed = 0
+			_speed = 0
 		
 	return
 
-func getVelocity():
-	return Vector2.RIGHT * speed
+func getVelocity() -> Vector2:
+	return Vector2.RIGHT * _speed
 
-func getBounceDirectionFromPosition(position):
+func getBounceDirectionFromPosition(position) -> Vector2:
 	var collider := $Collision as CollisionShape2D
 	var shape := collider.shape as RectangleShape2D
 	var width = shape.extents.x
