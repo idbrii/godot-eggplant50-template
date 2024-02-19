507d6c937284f285c76fa1f1b0890d2c61612856
David Briscoe <idbrii@gmail.com>
2024-02-19 09:06:48 -0800

    shared: Move input device checking to Eggplant
    Now games can call Eggplant.get_input_icons() to get the icons for the
    current input. Also means that the last input device you used will be
    detected instead of only input received while the widget is active.

--- shared/input/input_widget.gd
+++ shared/input/input_widget.gd
@@ -1,15 +1,5 @@
 extends Control
 
-var devices = {
-	keyboard = preload("res://shared/input/icon_keyboard.tres"),
-	nintendo = preload("res://shared/input/icon_nintendo.tres"),
-	playstation = preload("res://shared/input/icon_playstation.tres"),
-	xbox = preload("res://shared/input/icon_xbox.tres"),
-}
-
-var current_device = ""
-var last_input_joy = 0
-
 
 func set_game_def(def):
 	$"%Actions/primary/Label".text = _get_label(def.input_primary_action, "Primary")
@@ -19,72 +9,38 @@ func set_game_def(def):
 	$move_cardinal/Label.text = $move_omni/Label.text
 
 
+static func ok(err):
+	assert(err == OK)
+
+
+func _ready():
+	ok(Eggplant.connect("input_device_changed", self, "_on_input_device_changed"))
+	ok(self.connect("visibility_changed", self, "_on_visibility_changed"))
+
+
 func _get_label(label, default):
 	if label and label.length() > 0:
 		return label
 	return default
 
 
-func _input(event):
-	var is_gamepad_device = event is InputEventJoypadButton
-	var want_gamepad = is_gamepad_device
-	var is_mouse_drift = event is InputEventMouseMotion and event.speed.length_squared() < 3000
-	if event is InputEventJoypadMotion:
-		is_gamepad_device = true
-		# Big deadzone for auto switching visuals.
-		want_gamepad = event.axis_value > 0.4
-	if want_gamepad and Input.is_joy_known(event.device):
-		last_input_joy = event.device
-	elif not is_gamepad_device and not is_mouse_drift:
-		# Only disable gamepad if we definitely didn't receive input from a gamepad.
-		last_input_joy = -1
+func _on_visibility_changed():
+	if visible:
+		_on_input_device_changed("", Eggplant.get_input_icons())
 
 
-func get_device():
-	if Input.is_joy_known(last_input_joy):
-		var name = Input.get_joy_name(last_input_joy)
-		var appearance = get_simplified_device_name(name)
-		#~ printt("Detected", name, "as", appearance)
-		return appearance
-	return "keyboard"
+func _on_input_device_changed(_new_device, icons):
+	$"%Actions/primary/Icon".texture = icons.primary
+	$"%Actions/secondary/Icon".texture = icons.secondary
+	$"%pause/Icon".texture = icons.pause
+	var has_omni = icons.move_omni != null
+	$move_omni.visible = has_omni
+	$move_cardinal.visible = not has_omni
+	if icons.move_omni:
+		$move_omni/Icon.texture = icons.move_omni
+	else:
+		$move_cardinal/upper/up.texture = icons.move_up
+		$move_cardinal/lower/left.texture = icons.move_left
+		$move_cardinal/lower/down.texture = icons.move_down
+		$move_cardinal/lower/right.texture = icons.move_right
 
-
-func _process(_dt):
-	var new_device = get_device()
-	if new_device != current_device:
-		current_device = new_device
-		var icons = devices[current_device]
-		$"%Actions/primary/Icon".texture = icons.primary
-		$"%Actions/secondary/Icon".texture = icons.secondary
-		$"%pause/Icon".texture = icons.pause
-		var has_omni = icons.move_omni != null
-		$move_omni.visible = has_omni
-		$move_cardinal.visible = not has_omni
-		if icons.move_omni:
-			$move_omni/Icon.texture = icons.move_omni
-		else:
-			$move_cardinal/upper/up.texture = icons.move_up
-			$move_cardinal/lower/left.texture = icons.move_left
-			$move_cardinal/lower/down.texture = icons.move_down
-			$move_cardinal/lower/right.texture = icons.move_right
-
-
-
-# https://github.com/nathanhoad/godot_input_helper/blob/70d6513d3ba3dce463ed12cdd412e451aac95975/addons/input_helper/input_helper.gd#L61C1-L79C25
-func get_simplified_device_name(raw_name: String) -> String:
-	match raw_name:
-		"XInput Gamepad", "Xbox Series Controller", "Xbox 360 Controller", "Xbox One Controller":
-					return "xbox"
-
-		"Sony DualSense", "PS5 Controller", "PS4 Controller", "Nacon Revolution Unlimited Pro Controller":
-					return "playstation"
-
-		"Switch":
-			return "nintendo"
-		"Joy-Con (L)":
-			return "nintendo"
-		"Joy-Con (R)":
-			return "nintendo"
-
-		_:
-			return "xbox"
