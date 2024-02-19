4f72b0c0f7ceb35d571b4a1f52b8f5e01d194ac1
David Briscoe <idbrii@gmail.com>
2024-02-19 09:46:43 -0800

    shared: Support dark on light in input_widget
    Fix white text is hard to read on loading screen.
    
    Add an export to input_widget to allow a dark theme for the text.

--- shared/input/input_widget.gd
+++ shared/input/input_widget.gd
@@ -1,5 +1,8 @@
 extends Control
 
+enum Theme { DarkOnLight, LightOnDark, }
+export(Theme) var contrast_theme := Theme.LightOnDark
+
 
 func set_game_def(def):
 	$"%Actions/primary/Label".text = _get_label(def.input_primary_action, "Primary")
@@ -16,6 +19,14 @@ static func ok(err):
 func _ready():
 	ok(Eggplant.connect("input_device_changed", self, "_on_input_device_changed"))
 	ok(self.connect("visibility_changed", self, "_on_visibility_changed"))
+
+	if contrast_theme == Theme.DarkOnLight:
+		var c = Color("#372538")
+		$"%Actions/primary/Label".add_color_override("font_color", c)
+		$"%Actions/secondary/Label".add_color_override("font_color", c)
+		$"%pause/Label".add_color_override("font_color", c)
+		$move_omni/Label.add_color_override("font_color", c)
+		$move_cardinal/Label.add_color_override("font_color", c)
 
 
 func _get_label(label, default):
