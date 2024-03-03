73de6ff3143fbfb4065a4d3874666fafac8358f9
Jim Kang <jimkang@gmail.com>
2024-03-03 15:50:52 -0500

    Fruit now gravitates to center.

--- games/ripe_place/ripe-place.gd
+++ games/ripe_place/ripe-place.gd
@@ -1,18 +1,21 @@
 extends Node2D
 
 export(PackedScene) var fruit_scene
+export(Vector2) var gravity_center
+export(float) var grav_units_sq_per_sec
+var fruit
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func _process(_delta):
+	var to_gcenter = gravity_center - fruit.position
+	var to_gcenter_mag = to_gcenter.normalized() * grav_units_sq_per_sec
+	print('mag', to_gcenter_mag)
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var fruit = fruit_scene.instance()
+	fruit = fruit_scene.instance()
 	fruit.position = Vector2(64, 64)
-	add_child(fruit)
+	$gravityArea.add_child(fruit)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
