50b3a12bd5bd6a2f6de8a01ab4dcc8b5c60068cd
Jim Kang <jimkang@gmail.com>
2024-03-04 10:24:00 -0500

    Comment cleaning.

--- games/ripe_place/Fruit_Spawn_Scene.gd
+++ games/ripe_place/Fruit_Spawn_Scene.gd
@@ -3,8 +3,6 @@ export(PackedScene) var fruit_scene
 
 var occupants = 0
 var gravityArea
-#var dropping_right_now = false
-#var fruit
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,18 +14,9 @@ func _process(_delta):
 	print('occupants', occupants)
 
 func drop_new_fruit():
-#	dropping_right_now = true
-#	print('Dropping fruit')
 	var fruit = fruit_scene.instance()
 	fruit.position = self.position
-#	fruit.connect('tree_entered', self, 'on_fruit_entered_tree')
 	gravityArea.add_child(fruit)
-
-#func on_fruit_entered_tree():
-#	dropping_right_now = false
-#	fruit.disconnect('tree_entered', self, 'on_fruit_entered_tree')
-#	fruit = null
-#	print('Drop done.')
 
 func _on_SpawnArea_body_entered(body):
 	occupants += 1
