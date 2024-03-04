16d83c12e3c78c606289514ea3d2fe2e9303528c
Jim Kang <jimkang@gmail.com>
2024-03-03 22:35:05 -0500

    Avoid double spawning fruit by doing in ready() and the body exited handler. The process one would get called before physics changes from the previous call took place.

--- games/ripe_place/Fruit_Spawn_Scene.gd
+++ games/ripe_place/Fruit_Spawn_Scene.gd
@@ -3,24 +3,37 @@ export(PackedScene) var fruit_scene
 
 var occupants = 0
 var gravityArea
+#var dropping_right_now = false
+#var fruit
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	gravityArea = self.get_parent().find_node('gravityArea')
-
+	drop_new_fruit()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	print('occupants', occupants)
-	if occupants < 1:		
-		drop_new_fruit()
 
 func drop_new_fruit():
+#	dropping_right_now = true
+#	print('Dropping fruit')
 	var fruit = fruit_scene.instance()
 	fruit.position = self.position
+#	fruit.connect('tree_entered', self, 'on_fruit_entered_tree')
 	gravityArea.add_child(fruit)
-	
+
+#func on_fruit_entered_tree():
+#	dropping_right_now = false
+#	fruit.disconnect('tree_entered', self, 'on_fruit_entered_tree')
+#	fruit = null
+#	print('Drop done.')
+
 func _on_SpawnArea_body_entered(body):
 	occupants += 1
 
 func _on_SpawnArea_body_exited(body):
 	occupants -= 1
+	#if occupants < 1 && !dropping_right_now:
+	if occupants < 1: 
+		drop_new_fruit()
