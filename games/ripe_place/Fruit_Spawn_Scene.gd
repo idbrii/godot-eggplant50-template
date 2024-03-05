524785537e595eb67a398756ead81140fa558bc6
Jim Kang <jimkang@gmail.com>
2024-03-05 13:09:46 -0500

    Spawn new fruit after a variable amount of time. Fruit still ripens really quickly.

--- games/ripe_place/Fruit_Spawn_Scene.gd
+++ games/ripe_place/Fruit_Spawn_Scene.gd
@@ -7,8 +7,8 @@ var gravityArea
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	gravityArea = self.get_parent().find_node('gravityArea')
-	drop_new_fruit()
-#	$Timer.start()
+	var first_spawn_time = rand_range(1.0, 30.0)
+	$spawn_timer.start(first_spawn_time)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(_delta):
@@ -28,10 +28,10 @@ func drop_new_fruit():
 # The bat has an area, not a body, and does not
 # move via physics.
 # Also, if you make gravityArea monitorable, it will count as an area shape that never leaves.
-func _on_SpawnArea_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+func _on_SpawnArea_area_shape_entered(_area_rid, _area, _area_shape_index, _local_shape_index):
 	increment_occupants()
 
-func _on_SpawnArea_area_shape_exited(area_rid, area, area_shape_index, local_shape_index):
+func _on_SpawnArea_area_shape_exited(_area_rid, _area, _area_shape_index, _local_shape_index):
 	decrement_occupants()
 
 func increment_occupants():
@@ -41,10 +41,16 @@ func increment_occupants():
 func decrement_occupants():
 	occupants -= 1
 	#if occupants < 1 && !dropping_right_now:
-	if occupants < 1: 
-		drop_new_fruit()
+	if occupants < 1:
+		$spawn_timer.start(rand_range(1, 3))
+#		drop_new_fruit()
 #	print('spawn area exited: ', occupants)
 #	var overlapping_areas = $SpawnArea.get_overlapping_areas()
 #	print('overlapping_areas: ', overlapping_areas)
 #	var overlapping_bodies = $SpawnArea.get_overlapping_bodies()
 #	print('overlapping_bodies: ', overlapping_bodies)
+
+func _on_spawn_timer_timeout():
+	print('timer done for ', self)
+#	self.call_deferred('drop_new_fruit')
+	self.drop_new_fruit()
