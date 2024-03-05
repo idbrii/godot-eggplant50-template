f9868b969b4a6f081fcb86f5bd214ee6be0730ae
Jim Kang <jimkang@gmail.com>
2024-03-04 22:05:42 -0500

    Indicate bat is over fruit by scaling it up (for now). Start action over fruit.

--- games/ripe_place/ripe-place.gd
+++ games/ripe_place/ripe-place.gd
@@ -2,10 +2,14 @@ extends Node2D
 export(PackedScene) var BatScene
 export(Vector2) var screen_size
 export(int) var move_size
- 
+
 var bat
 
 func _process(_delta):
+	handle_bat_move()
+	handle_bat_action()
+
+func handle_bat_move():
 	var x = 0
 	var y = 0
 	if Input.is_action_just_released("move_left"):
@@ -16,15 +20,17 @@ func _process(_delta):
 		y -= 1
 	if Input.is_action_just_released("move_down"):
 		y += 1
-	
+
 	var move = Vector2(x * move_size, y * move_size)
-		
+
 	bat.position += move
 	bat.position.x = clamp(bat.position.x, 0, screen_size.x)
 	bat.position.y = clamp(bat.position.y, 0, screen_size.y)
-#	# Using get_axis will have a cross deadzone.
-#	var move_x := Input.get_axis("move_left", "move_right")
-#	var move_y := Input.get_axis("move_up", "move_down")
+
+func handle_bat_action():
+	if Input.is_action_just_released("action1"):
+		var intersecting_bodies = bat.get_node('Area2D').get_overlapping_bodies()
+		print('colliding: ', intersecting_bodies)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
