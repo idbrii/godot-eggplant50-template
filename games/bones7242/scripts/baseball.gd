be13ee9b9a9bf7d51e3ee191e2f25c67b5d39c00
family PC <bittner.w@gmail.com>
2024-03-08 20:01:48 -0800

    started working on the 3d scaling script

--- games/bones7242/scripts/baseball.gd
+++ games/bones7242/scripts/baseball.gd
@@ -1,12 +1,10 @@
 extends Area2D
 
-var angular_speed = PI
-var starting_windup_speed = 10
-var windup_speed = starting_windup_speed
-var max_windup_speed = 200
-
 const State = {HELD = 0, WINDUP = 1, RELEASED = 2, GROUNDED = 3}
 # Access values with State.HELD, etc.
+
+var room_height = 360 #TO do replace with actual propery
+var room_width = 640 #TO do replace with actual propery
 
 var initial_state = State.HELD
 var current_state : int
@@ -14,11 +12,19 @@ var last_state : int
 # tbd: set up with enter and exit state functions to set state.
 
 var power = 0
-var power_increment = 40
-var grav = power_increment
+var grav = 10
 var max_fall_speed = 100
 
-var h_move_speed = 8
+var unprojectedX = 260
+var unprojectedY = 300 # should start up off the ground a bit
+var unprojectedZ = 2
+var xVelocity = 0 # will be set by creator
+var yVelocity = 0 # will be set by creator
+var zVelocity = 0 # will be set by creator
+var yGravity = 0 # set by creator, should be set globally so all have same gravity
+
+var projectedCoordinates : Array
+var rotationSpeed = 5; # must be multiple of rotateUprightSpeed and 
 
 func change_state(new_state):
 	# store the current state as last state, if it exists
@@ -28,9 +34,100 @@ func change_state(new_state):
 	current_state = new_state
 	
 	pass
+	
+func change_state_held():
+	last_state = current_state
+	change_state(State.HELD)
+	#get_node("spr_baseball").visible = false
+	pass
+
+func change_state_released():
+	last_state = current_state
+	change_state(State.RELEASED)
+	#get_node("spr_baseball").visible = true
+	pass	
+
+func change_state_grounded():
+	last_state = current_state
+	change_state(State.GROUNDED)
+	power = 0 #reset power
+	pass
+	
+func determineZAsPercent (unprojectedZDistance) :
+	var unprojectedZasPerecentOfRoomHeight = 100 * unprojectedZDistance / (25 + unprojectedZDistance)
+	var zDistanceAsPercent = unprojectedZasPerecentOfRoomHeight / 100
+	return  zDistanceAsPercent
+	
+func convert3dCoordintesTo2d(x_cord, y_cord, z_cord):
+	var projectedX : int
+	var projectedY : int 
+	
+	var tX = unprojectedX # in room top left is 0,0, so tx = mouse_x
+	var tY = unprojectedY 
+	var tZ = unprojectedZ # in room top left is 0,0, so ty = room height - mouse_y
+	var t = [tX, tY, tZ]
+
+	# determine size of projected space (trapezoid)
+	var desiredHorizonWidth = 520
+	var desiredHorizonHeight = room_height - 32
+	#desiredHorizonYLocation = 100;  // remember, top of screen is 0, bottom is room_height.
+	var trTopLeft = [(room_width - desiredHorizonWidth)/2, desiredHorizonHeight]
+	var trTopRight = [room_width - ((room_width - desiredHorizonWidth)/2), desiredHorizonHeight]
+	var trBottomLeft = [0,0]
+	var trBottomRight = [640,0]
+
+	var HeightOfProjectedAtCenter = ((trTopLeft[1] - trBottomLeft[1]) + (trTopRight[1] - trBottomRight[1])) / 2
+	#HeightOfProjectedLeft = tTopRight[1] - tBottomRight[1];  // 2d height squished field, currently still 360.
+	#HeightOfProjectedRight = tTopRight[1] - tBottomRight[1]; // note: not used currently b/c no skewing or tilting
+	var WidthOfProjectedFar = trTopRight[0] - trTopLeft[0]
+	var WidthOfProjectedNear = trBottomRight[0] - trBottomLeft[0]
+	var minimumDepth = 0
+	var maximumDepth = HeightOfProjectedAtCenter
+
+
+	# DETERMINE Y in 2D space
+	var zDistanceAsPercent = determineZAsPercent(unprojectedZ)
+	#projectedY = room_height - (room_height * zDistanceAsPercent) - unprojectedY;
+	projectedY = room_height - (desiredHorizonHeight * zDistanceAsPercent) - unprojectedY;
+
+	# DETERMINE X in 2D space
+	# interpolate between two points representing two different widths at two different depths > [x (y value), y (width)] > [0,640] & [360, 520]
+	var WidthAtProjectedDepth = WidthOfProjectedNear + ((t[2] - minimumDepth)/(maximumDepth-minimumDepth)*(WidthOfProjectedFar-WidthOfProjectedNear)) # interpolates between the bottom and top widths, linearly to find the width at the given height (z)
+	var unusedSpaceLeftOfWidthAtDepth = (room_width - WidthAtProjectedDepth) / 2
+
+	var XAsPercentOfOriginalWidth = t[0]/room_width
+	var XPositionOnWidthAtDepth = XAsPercentOfOriginalWidth * WidthAtProjectedDepth
+
+	projectedX = unusedSpaceLeftOfWidthAtDepth + XPositionOnWidthAtDepth;
+	#clamp(x,0,room_width);// don't let x leave the screen left or screen right;
+		
+	return [projectedX, projectedY]
+	
+func set_3d_position():
+	var projectedCoordinates = convert3dCoordintesTo2d(unprojectedX, unprojectedY, unprojectedZ)
+	position.x = projectedCoordinates[0]
+	position.y = projectedCoordinates[1]
+
+func set_3d_position_from_coords (x_cord, y_cord, z_cord):
+	# update the coords
+	unprojectedX = x_cord
+	unprojectedY = y_cord
+	unprojectedZ = z_cord
+	# update the 3d position
+	set_3d_position()
+
+func determineImageScale (zDistance) :
+	var imageScale = determineZAsPercent(zDistance);
+	return  imageScale;
+		
+
+func set_3d_image_scale () :
+	scale = 
 
 func _ready():
-	change_state(initial_state)
+	change_state_held()
+	set_3d_position()
+	set_3d_image_scale()
 	pass
 	
 func _process(delta):
@@ -39,29 +136,7 @@ func _process(delta):
 	
 	match current_state:
 		State.HELD:
-			#print("i am HELD")
-			#wait around
-			# movement
-			var move_x := Input.get_axis("move_left", "move_right")
-			position.x += move_x * h_move_speed
-			#listen for change to windup
-			if Input.is_action_pressed("action1"):
-				change_state(State.WINDUP)
-				print("i am winding up")
-		State.WINDUP:
-			#print("i am WINDUP")
-			# do windup stuff
-			var potential_move = 1 * windup_speed * delta
-			if (position.y + potential_move) < 300:
-				position.y += potential_move
-				power += power_increment
-				print('power:' + str(power))
-				windup_speed += (max_windup_speed - windup_speed) / 2 #tweening to max
-			#listen for change to released
-			if Input.is_action_just_released("action1"):
-				print("i am RELEASED")
-				change_state(State.RELEASED)
-				windup_speed = starting_windup_speed #reset windup speed
+			pass
 		State.RELEASED:
 			# do released stuff
 			power -= grav
@@ -69,16 +144,14 @@ func _process(delta):
 			var next_movement = -1 * power * delta
 			if next_movement <= max_fall_speed:
 				position.y += next_movement # move ball the ball
-			
 			# also listen for hitting the ground
 			if position.y >= 300:
-				change_state(State.GROUNDED)
-				power = 0 #reset power
+				change_state_grounded()
+				power = min_power # reset
 		State.GROUNDED:
 			#if Input.is_action_just_released("action1"):
 			if true: #placeholder - replace with timer after "strike" or hit or whatever.
-				change_state(State.HELD)
-				position.y = 199
+				change_state_held()
 		_:
 			print("I am not a baseball state I know of!")
 		
---
+++ games/bones7242/scripts/batter.gd
@@ -0,0 +1,101 @@
+extends Node2D
+
+const State = {IDLE = 0, CROUCH = 1, RELEASE = 2, WINDUP = 3, SWING = 4, FOLLOWTHROUGH = 5}
+# Access values with State.HELD, etc.
+
+var initial_state = State.IDLE
+var current_state : int
+var last_state : int
+
+var spr_batter_idle = preload("../sprites/1-idle.png")
+var spr_batter_crouch = preload("../sprites/2-crouch.png")
+var spr_batter_release = preload("../sprites/3-release.png")
+var spr_batter_windup = preload("../sprites/4-windup.png")
+var spr_batter_swing = preload("../sprites/5-swing.png")
+var spr_batter_followthrough = preload("../sprites/6-followthrough.png")
+
+var min_throw_power  = 0
+var throw_power = min_throw_power
+var max_throw_power = 100
+var throw_power_increment = 1
+
+var min_swing_power  = 0
+var swing_power = min_swing_power
+var max_swing_power = 100
+var swing_power_increment = 1
+
+func change_state(new_state):
+	# store the current state as last state, if it exists
+	if current_state:
+		last_state = current_state
+	
+	current_state = new_state
+	pass
+
+func check_for_hit():
+	#get position of ball
+	var baseball_object = $"../baseball_area2d"
+	#see if ball within radius of hitting sphere
+	var distance_to_ball_center = position.distance_to(baseball_object.position)
+	print('distance to ball = ' + str(distance_to_ball_center))
+	return distance_to_ball_center < 100
+
+func _ready():
+	change_state(initial_state)
+	get_node("Sprite").set_texture(spr_batter_idle)
+	pass
+	
+func _process(delta):
+	
+	match current_state:
+		State.IDLE:
+			#print("i am HELD")
+			#wait around
+			#listen for change to windup
+			if Input.is_action_pressed("action1"):
+				change_state(State.CROUCH)
+				get_node("Sprite").set_texture(spr_batter_crouch)
+		State.CROUCH:
+			# build up power
+			if throw_power < max_throw_power:
+				throw_power += throw_power_increment
+				print("throw power = " + str(throw_power))
+			# listen for release
+			if Input.is_action_just_released("action1"):
+				change_state(State.RELEASE)
+				get_node("Sprite").set_texture(spr_batter_release)
+				# affect the ball
+				throw_power = min_throw_power # reset
+				get_parent().get_node("baseball_area2d").change_state_released()
+				get_parent().get_node("baseball_area2d").power = throw_power * 100
+		State.RELEASE:
+			if Input.is_action_pressed("action1"):
+				change_state(State.WINDUP)
+				swing_power = min_swing_power
+				get_node("Sprite").set_texture(spr_batter_windup)
+		State.WINDUP:
+			# build up power
+			if swing_power < max_swing_power:
+				swing_power += swing_power_increment
+			print("swing power = " + str(swing_power))
+			#listen for change to released
+			if Input.is_action_just_released("action1"):
+				change_state(State.SWING)
+				get_node("Sprite").set_texture(spr_batter_swing)
+		State.SWING:
+			# check if a hit, 
+			# if so, then initiate the hit state on baseball
+			if check_for_hit():
+				print("YOU GOT A HIT!")
+				#to do: affect the ball
+			change_state(State.FOLLOWTHROUGH)
+			get_node("Sprite").set_texture(spr_batter_followthrough)
+		State.FOLLOWTHROUGH:
+			#when ball hits the floor...
+			change_state(State.IDLE)
+			get_node("Sprite").set_texture(spr_batter_idle)
+		_:
+			print("I am not a bat state I know of!")
+		
+	#print('power:' + str(power))
+	update()
